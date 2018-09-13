2d0bd2856776a3d5ea81440dd3ef2510dad72883
OldTeaOwl <lira_hertstrings@mail.ru>
2018-09-13 20:10:31 +0300

    map written

--- src/system/Map.java
+++ src/system/Map.java
@@ -1,5 +1,39 @@
 package system;
 
+import java.util.ArrayList;
+
+import org.newdawn.slick.Graphics;
+
 public class Map {
+	
+	private ArrayList<Elements> elements = new ArrayList<>();
+	
+	public Map() {
+		
+	}
+	
+	public void draw(Graphics g) {
+		for(Elements element: elements) element.draw(g);
+	}
+	
+	
+	
+	/* -- методы доступа, делегирующие запросы к ArrayList -- */
+	
+	public Elements getElement(int index) {
+		return elements.get(index);
+	}
+	
+	public int getIndex(Elements element) {
+		return elements.indexOf(element);
+	}
+	
+	public void removeElement(int index) {
+		elements.remove(index);
+	}
+
+	public void removeElement(Object obj) {
+		elements.remove(obj);
+	}
 
 }
